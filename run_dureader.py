f0c3df991d0c3fcf3e333ed6e0fe486d6209c4d3
why <hustwhy2000@foxmail.com>
2019-03-13 11:48:22 +0800

    why

--- run_dureader.py
+++ run_dureader.py
@@ -304,10 +304,7 @@ def read_dureader(input_file, is_training):
                 answer_docs = sample['answer_docs'][0]
                 most_related_documents = sample['documents'][answer_docs]
             except:
-                print("exceptfound")
-                print(answer_docs)
-                print(sample)
-                print("exceptend")
+                continue
             most_related_para_id = most_related_documents['most_related_para']
             most_related_para_seg = most_related_documents['segmented_paragraphs'][most_related_para_id]
             doc_tokens = most_related_para_seg
